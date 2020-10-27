2817bfdd9521c7a0031635ab4873c26109ea3d4e
MatiMartinez <matia.martine2018@gmail.com>
2020-10-27 17:51:00 -0300

    Updates

---
+++ precache-manifest.9aca3815196cc0a93271e70f337adb88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d220892e2a2b1de6f7874d4ec33806dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34c92879ccb7c58b12cd",
+    "url": "/static/css/main.35be9552.chunk.css"
+  },
+  {
+    "revision": "8c2b4d51404eff47aedf",
+    "url": "/static/js/2.3298bf50.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3298bf50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c92879ccb7c58b12cd",
+    "url": "/static/js/main.7748d0ed.chunk.js"
+  },
+  {
+    "revision": "2d1b0eb762181eef8974",
+    "url": "/static/js/runtime-main.0f7178bd.js"
+  },
+  {
+    "revision": "4919e97122b6c93de2ff9c05ab73f341",
+    "url": "/static/media/classroom.4919e971.jpg"
+  },
+  {
+    "revision": "583070f2af10d709dce383ed137cb319",
+    "url": "/static/media/developer.583070f2.jpg"
+  },
+  {
+    "revision": "ac976075a5d3d52bf275b9409e3a7d49",
+    "url": "/static/media/edit-video.ac976075.jpg"
+  },
+  {
+    "revision": "dbd790934cfea2b2ec9affced3fe4123",
+    "url": "/static/media/github-profiles.dbd79093.svg"
+  },
+  {
+    "revision": "c07a4434710a79adf8684b035c7de7d7",
+    "url": "/static/media/image-search.c07a4434.svg"
+  },
+  {
+    "revision": "0af464a85f27e920b05de9a399268176",
+    "url": "/static/media/mplay.0af464a8.svg"
+  },
+  {
+    "revision": "c8eef3bcb39256800e7ac7b3d3a0f944",
+    "url": "/static/media/simple-cart.c8eef3bc.svg"
+  }
+]);
